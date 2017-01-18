ac0d7ece5d69fe45ef97ce3dea7ea62fde056e4c
海瑞 <hellohairui@gmail.com>
2017-01-18 10:54:00 +0800

    创建工程

---
+++ TMChart/TMChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMChart
+//
+//  Created by harry on 17/1/18.
+//  Copyright © 2017年 timer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
